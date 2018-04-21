c2e5fc6476371737b56336c070abde3ebbd85c08
andreoidb64 <andreoidb64@gmail.com>
2018-04-21 02:28:42 +0200

    Create DynDnsRefresh.sh

---
+++ DynDnsRefresh.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+################################################################################
+# file: /etc/DynDnsRefresh.sh
+# auth: andreoidb64
+# date: 20180421
+#
+# NOTE: Run from cron
+#
+################################################################################
+
+InetIP=999.999.999.999
+
+InetIP=$(/usr/bin/wget -O - http://ident.me/ 2>/dev/null)
+/bin/ping -c 3 $InetIP || exit 1
+
+echo "To do: Refresh DNS record: $InetIP"
